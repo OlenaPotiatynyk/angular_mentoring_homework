68d087a4958067b4498ca265ee39aa29b7f2f718
Olena_Potiatynyk <Olena_Potiatynyk@epam.com>
2019-10-19 08:14:17 +0300

    add courses page

---
+++ src/app/courses/courses-page/courses-page.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+
+export interface Course {
+  id: string;
+  title: string;
+  creationDate: string;
+  duration: number;
+  description: string;
+}
+
+export interface User {
+  id: string;
+  firstName: string;
+  lastName: string;
+}
+
+@Component({
+  selector: 'app-courses-page',
+  templateUrl: './courses-page.component.html',
+  styleUrls: ['./courses-page.component.scss']
+})
+export class CoursesPageComponent implements OnInit {
+  private courses: Course[] = [
+    {
+      id: '1',
+      title: 'course 1',
+      creationDate: 'some date',
+      duration: 60,
+      description: 'dedscription'
+    }
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
