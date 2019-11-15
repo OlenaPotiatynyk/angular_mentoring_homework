b2d99a0faa4e9fe5f8b03b0aedffcdb9ce73ec0c
Olena_Potiatynyk <Olena_Potiatynyk@epam.com>
2019-11-15 14:48:57 +0200

    add auth service

---
+++ src/app/core/auth.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor() { }
+
+  login() {
+    if (this.isAuthenticated()) {
+      return;
+    }
+
+    const userData = {
+      email: 'test@mail.com',
+      password: '',
+      token: Math.random()
+    };
+    localStorage.setItem('user', JSON.stringify(userData));
+    console.log('log in successfully');
+  }
+
+  logout() {
+    if (!this.isAuthenticated()) {
+      return;
+    }
+
+    localStorage.removeItem('user');
+    console.log('log out successfully');
+  }
+
+  isAuthenticated(): boolean {
+    return !!localStorage.getItem('user');
+  }
+
+  getUserInfo(): string {
+    return localStorage.getItem('user').match(/([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+)/gi)[0];
+  }
+}
