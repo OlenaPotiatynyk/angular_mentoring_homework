b2d3b37325c30444365e672c8fde4df8a74eb6ec
Olena_Potiatynyk <Olena_Potiatynyk@epam.com>
2019-11-22 12:34:21 +0200

    add handlers

--- src/app/courses/add-course-page/add-course-page.component.ts
+++ src/app/courses/add-course-page/add-course-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'app-add-course-page',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-course-page.component.scss']
 })
 export class AddCoursePageComponent implements OnInit {
+  @Output() cancel = new EventEmitter();
+
   title = '';
   description = '';
   date = '';
@@ -17,8 +19,12 @@ export class AddCoursePageComponent implements OnInit {
   ngOnInit() {
   }
 
-  submit(): void {
+  onSubmit(): void {
     console.log('Title: ' + this.title + ' Description: '
       + this.description + ' Date: ' + this.date + ' Duration: ' + this.duration + ' Author: ' + this.authors);
+  }
+
+  onCancel(): void {
+    this.cancel.emit();
   }
 }
