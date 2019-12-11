af5067c6d4bf3defcf553ebbed4347ca17c6b522
Olena_Potiatynyk <Olena_Potiatynyk@epam.com>
2019-12-11 16:40:02 +0200

    add auth interceptor

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,7 +4,9 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { LogoComponent } from './logo/logo.component';
 import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
-import {RouterModule} from "@angular/router";
+import { RouterModule } from '@angular/router';
+
+import { httpInterceptorProviders } from './http-interceptors/http-interceptor-providers';
 
 @NgModule({
   declarations: [HeaderComponent, FooterComponent, LogoComponent, BreadcrumbsComponent],
@@ -17,6 +19,9 @@ import {RouterModule} from "@angular/router";
     FooterComponent,
     LogoComponent,
     BreadcrumbsComponent
+  ],
+  providers: [
+    httpInterceptorProviders
   ]
 })
 export class CoreModule { }
