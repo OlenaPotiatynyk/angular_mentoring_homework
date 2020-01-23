f7170318312c76a72e906cb669985091d4ea0c53
Olena_Potiatynyk <Olena_Potiatynyk@epam.com>
2020-01-23 18:06:49 +0200

    Make all fields required

--- src/app/courses/add-and-edit-course-page/add-and-edit-course-page.component.ts
+++ src/app/courses/add-and-edit-course-page/add-and-edit-course-page.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Validators, FormBuilder } from '@angular/forms';
 
 import { CoursesService } from '../courses.service';
 
@@ -10,19 +10,23 @@ import { CoursesService } from '../courses.service';
   styleUrls: ['./add-and-edit-course-page.component.scss']
 })
 export class AddAndEditCoursePageComponent implements OnInit {
-    courseForm = new FormGroup({
-        name: new FormControl('', [Validators.required, Validators.maxLength(50)]),
-        description: new FormControl('', [Validators.required, Validators.maxLength(500)]),
-        date: new FormControl(''),
-        length: new FormControl(null),
-        authors: new FormControl('', Validators.required),
-    });
+  courseForm = this.fb.group({
+    name: ['', [Validators.required, Validators.maxLength(50)]],
+    description: ['', [Validators.required, Validators.maxLength(500)]],
+    date: ['', [Validators.required]],
+    length: [null, [Validators.required]],
+    authors: ['', [Validators.required]]
+  });
 
   routeParams: {
     id?: number;
   } = {};
 
-  constructor(private coursesService: CoursesService, private router: Router, private activatedRoute: ActivatedRoute) { }
+  constructor(private coursesService: CoursesService,
+              private router: Router,
+              private activatedRoute: ActivatedRoute,
+              private fb: FormBuilder) {
+  }
 
   public ngOnInit(): void {
     this.activatedRoute.params.subscribe((routeParams) => {
@@ -34,17 +38,17 @@ export class AddAndEditCoursePageComponent implements OnInit {
     }
   }
 
-    get f() {
-        return this.courseForm.controls;
-    }
+  get controls() {
+    return this.courseForm.controls;
+  }
 
   public onSubmit(): void {
     const data = {
-        name: this.courseForm.value.name,
-        description: this.courseForm.value.description,
-        date: this.courseForm.value.date,
-        length: +this.courseForm.value.length,
-        authors: this.courseForm.value.authors.split(',').map((fullName, index) => {
+      name: this.courseForm.value.name,
+      description: this.courseForm.value.description,
+      date: this.courseForm.value.date,
+      length: +this.courseForm.value.length,
+      authors: this.courseForm.value.authors.split(',').map((fullName, index) => {
         const author = fullName.trim().split(' ');
         return {
           id: index + 1,
@@ -66,15 +70,15 @@ export class AddAndEditCoursePageComponent implements OnInit {
     this.coursesService.getItemById(this.routeParams.id)
       .subscribe(resp => {
         if (resp.length > 0) {
-            this.courseForm.patchValue({
-                name: resp.name,
-                description: resp.description,
-                date: resp.date,
-                length: resp.length,
-                authors: resp.authors && resp.authors.length > 0
-                    ? resp.authors.map(author => author.name + ' ' + author.lastName).join(', ')
-                    : ''
-            });
+          this.courseForm.patchValue({
+            name: resp.name,
+            description: resp.description,
+            date: resp.date,
+            length: resp.length,
+            authors: resp.authors && resp.authors.length > 0
+              ? resp.authors.map(author => author.name + ' ' + author.lastName).join(', ')
+              : ''
+          });
         }
       });
   }
